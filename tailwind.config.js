99c258106f15db6eae53724bb0276528765f8078
achu <anchovy999@gmail.com>
2025-06-10 00:40:17 +0900

    TailwindCSSの設定

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // srcディレクトリ以下のすべての関連ファイルを対象にする
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
